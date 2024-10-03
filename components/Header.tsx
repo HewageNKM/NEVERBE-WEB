0ab76ad84a98a7ac11f59bc54ef6b28543865ff1
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-10-03 22:41:38 +0530

    add header menu urls

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,10 +18,12 @@ const Header = () => {
         <header className="w-full mt-4 relative" onMouseLeave={() => setShowBrands(false)}>
             <ul className="flex-row z-40 hidden mt-3 absolute lg:flex w-full justify-center items-center">
                 <div className="flex z-50 flex-row gap-10">
-                    <Link onMouseEnter={() => setShowBrands(true)} href=""
+                    <Link href="/products" className="text-3xl hover:text-primary transition-all font-bold tracking-widest">
+                        <h2>Shop Now</h2></Link>
+                    <Link onMouseEnter={() => setShowBrands(true)} href="/products"
                           className="text-3xl hover:text-primary transition-all font-bold tracking-widest">
                         <h2>Brands</h2></Link>
-                    <Link href="" className="text-3xl hover:text-primary transition-all font-bold tracking-widest">
+                    <Link href="/products" className="text-3xl hover:text-primary transition-all font-bold tracking-widest">
                         <h2>Accessories</h2></Link>
                 </div>
             </ul>
