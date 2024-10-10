3a5979310bd30b3ab4baa32fe89390f781922075
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-10-10 19:23:06 +0530

    Fail page bug fix

--- app/shop/checkout/fail/page.tsx
+++ app/shop/checkout/fail/page.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Link from "next/link";
-import SuccessAnimationComponents from "@/app/shop/checkout/success/components/SuccessAnimationComponents";
+import FailAnimationComponent from "@/app/shop/checkout/fail/components/FailAnimationComponent";
 
 const Page = () => {
 
     return (
         <main className="w-full md:mt-20 mb-10 lg:mt-28 mt-16 flex justify-center items-center">
             <div className="w-fit h-[18rem] p-8 flex flex-col gap-5 justify-center items-center">
-                <SuccessAnimationComponents/>
+                <FailAnimationComponent/>
                 <div
                     className="font-bold gap-3 flex flex-col md:text-xl lg:text-2xl justify-center items-center capitalize text-green-500 text-lg">
                     <h3 className="text-center">
-                        Sorry, Your order placed has been failed!
+                        Sorry, Your order placement has been failed!
                     </h3>
                     <Link href="/" className="text-blue-500 md:text-lg text-sm">
                         Go back to home
