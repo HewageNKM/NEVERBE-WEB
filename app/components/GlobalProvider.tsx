3765aa4daf5068af259cbd5902cfffdf571282ed
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-10-12 00:07:43 +0530

    Remove loading screen

--- app/components/GlobalProvider.tsx
+++ app/components/GlobalProvider.tsx
@@ -1,7 +1,6 @@
 "use client"
-import React, {ReactNode, useEffect, useState} from 'react';
+import React, {ReactNode, useEffect} from 'react';
 import {signUser} from "@/firebase/firebaseClient";
-import LoadingScreen from "@/components/LoadingScreen";
 import {AppDispatch, RootState} from "@/redux/store";
 import {useDispatch, useSelector} from "react-redux";
 import {initializeCart} from "@/redux/cartSlice/cartSlice";
@@ -11,30 +10,23 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const GlobalProvider = ({children}: { children: ReactNode }) => {
-    const [isLoading, setIsLoading] = useState(true)
     const dispatch: AppDispatch = useDispatch();
     const showCart = useSelector((state: RootState) => state.cartSlice.showCart);
 
     useEffect(() => {
-        signUser().then(() => {
-            setIsLoading(false)
-        }).catch(() => {
-            setIsLoading(false)
-        });
+        signUser();
         dispatch(initializeCart())
     });
 
     return (
         <main className="w-full relative flex flex-col justify-between h-full overflow-clip">
-            {isLoading ? <LoadingScreen/> :
-                <>
-                    <Header/>
-                    {children}
-                    <Footer/>
-                    <AnimatePresence>
-                        {showCart && (<Cart/>)}
-                    </AnimatePresence>
-                </>}
+
+            <Header/>
+            {children}
+            <Footer/>
+            <AnimatePresence>
+                {showCart && (<Cart/>)}
+            </AnimatePresence>
         </main>
     );
 };
