61f1dbd19b945b0b8a6aa6344d2d4228db77e367
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-12-09 10:30:22 +0530

    Optimized margin

--- app/components/Brands.tsx
+++ app/components/Brands.tsx
@@ -4,7 +4,7 @@ import { brands } from "@/constants";
 const Brands = () => {
     return (
         <section className="w-full bg-slate-100" aria-labelledby="brands-section">
-            <h2 id="brands-section" className="text-center text-4xl md:text-5xl lg:text-6xl font-bold mt-3 mb-4">Our Brands</h2>
+            <h2 id="brands-section" className="text-center text-4xl md:text-5xl lg:text-6xl font-bold md:mt-8 mt-4 mb-4">Our Brands</h2>
             <div className="p-8 mt-8">
                 <ul className="flex w-full mt-5 flex-wrap gap-10 flex-row justify-center items-center" role="list">
                     {brands.map((item, index) => (
--- app/shop/products/[itemId]/components/SimilarProducts.tsx
+++ app/shop/products/[itemId]/components/SimilarProducts.tsx
@@ -6,9 +6,9 @@ import EmptyState from "@/components/EmptyState";
 const SimilarProducts = ({ items }: { items: Item[] }) => {
     return (
         <section className="w-full mt-10">
-            <div className="px-8 py-8">
+            <div>
                 <div>
-                    <h2 className="text-4xl font-bold">Similar Products</h2>
+                    <h2 className="md:text-4xl text-2xl font-bold">Similar Products</h2>
                 </div>
                 <div className="mt-8 overflow-x-auto hide-scrollbar">
                     {items.length > 0 ? ( // Render items
