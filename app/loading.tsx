d55bc673eb3605110acd7db83911674291cd9f31
Ethan Blake <107237482+EthanBlake00@users.noreply.github.com>
2025-02-10 14:41:35 +0530

    Change the loading.tsx animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <main className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-md">
-            <div className="flex flex-col items-center">
-                <div className="animate-spin rounded-full lg:h-32 h-24 w-24 lg:w-32 border-t-4 border-b-4 border-primary"></div>
-                <h1 className="lg:text-2xl text-xl font-bold mt-4 text-white">Loading...</h1>
+        <main className="fixed inset-0 z-30 flex items-center justify-center bg-black/30 backdrop-blur-md">
+            <div className="absolute w-full h-full inset-0 bg-white/70 backdrop-blur-md flex items-center justify-center z-30">
+                <div className="w-12 h-12 border-4 border-t-4 border-gray-300 border-t-primary-100 rounded-full animate-spin"></div>
             </div>
         </main>
     );
