441775bfb950c37c98031e85a74c4a80238b3888
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2025-11-05 21:15:41 +0530

    Change Hero image style

--- app/components/ImagesSlider.tsx
+++ app/components/ImagesSlider.tsx
@@ -1,41 +1,46 @@
 "use client";
-import React from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import {Autoplay, Pagination} from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
 import Image from "next/image";
-import {Slide} from "@/interfaces";
+import { Slide } from "@/interfaces";
 
-const ImagesSlider = ({images}: { images: Slide[] }) => {
-    return (
-        <Swiper
-            autoplay={{
-                delay: 5000,
-                disableOnInteraction: true,
-            }}
-            pagination={{
-                dynamicBullets: true,
-            }}
-            modules={[Pagination, Autoplay]}
-            className="w-full relative mt-5"
-        >
-            {images.map((image) => (
-                <SwiperSlide key={image.id} className="w-full">
-                    <figure className="w-full h-full">
-                        <Image
-                            width={1200}
-                            height={628}
-                            src={image.url}
-                            alt={image.fileName || 'Image slideshow'} // Use a fallback if fileName is not available
-                            className="object-cover w-full"
-                            loading="lazy" // Lazy load images
-                        />
-                    </figure>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+const ImagesSlider = ({ images }: { images: Slide[] }) => {
+  return (
+    <Swiper
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: true,
+      }}
+      pagination={{
+        clickable: true,
+        el: ".custom-pagination",
+      }}
+      modules={[Pagination, Autoplay]}
+      className="w-full relative mt-5"
+    >
+      {images.map((image) => (
+        <SwiperSlide key={image.id} className="w-full">
+          <figure className="w-full p-4">
+            <div className="overflow-hidden rounded-2xl shadow-md">
+              <Image
+                width={1200}
+                height={628}
+                src={image.url}
+                alt={image.fileName || "Image slideshow"}
+                className="object-cover w-full"
+                loading="lazy"
+              />
+            </div>
+          </figure>
+        </SwiperSlide>
+      ))}
+      {/* Custom Pagination */}
+      <div className="custom-pagination mt-3 mb-2 flex justify-center gap-3"></div>
+    </Swiper>
+  );
 };
 
 export default ImagesSlider;
