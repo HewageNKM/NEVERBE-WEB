d37daa526eec07d1b61af6883d7a4c365cd949f2
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2025-11-05 15:28:30 +0530

    Remove Skelton

--- components/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Skeleton = () => {
-    return (
-        <div className="animate-pulse rounded-md bg-gray-400 w-full h-full"></div>
-    );
-};
-
-export default Skeleton;
